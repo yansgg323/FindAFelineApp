f8aac8933c930a3a0a3d308af1525448445da050
yansgg323 <yanagencheva32@gmail.com>
2025-04-13 12:03:33 +0300

    added some basic stuff

---
+++ src/FindAFelineApp.Data/Entities/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAFelineApp.Data.Entities
+{
+    public class Cat : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public string Personality { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
