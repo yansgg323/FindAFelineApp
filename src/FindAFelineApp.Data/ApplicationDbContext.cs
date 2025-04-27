ec0adc942b598bfe0d4feae89b3e0d5b49db4957
yansgg323 <yanagencheva32@gmail.com>
2025-04-27 14:55:13 +0300

    added some stuff in data (app db context, migrations and repositories) zvkvdkvd :(

---
+++ src/FindAFelineApp.Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using FindAFelineApp.Data;
+using FindAFelineApp.Data.Entities
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FindAFelineApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<Cat> Cats { get; set; }
+    }
+}
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json")
+             .Build();
+
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        builder.UseSqlServer(connectionString);
+        return new ApplicationDbContext(builder.Options);
+    }
+}
