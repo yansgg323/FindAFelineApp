53068310fb602abe10bea347724a43df90219209
yansgg323 <yanagencheva32@gmail.com>
2025-05-03 16:10:27 +0300

    added abstractions, DTOs, service and profiles in services

---
+++ src/FindAFelineApp.Services/CatService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using FindAFelineApp.Data.Entities;
+using FindAFelineApp.Data.Repositories;
+using FindAFelineApp.Data.Repositories.Abstractions;
+using FindAFelineApp.Services.Abstractions;
+using FindAFelineApp.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAFelineApp.Services
+{
+    class CatService : IService
+    {
+        private readonly ICrudRepository<Cat> _catRepository;
+        private readonly IMapper _mapper;
+
+        public CatService(ICrudRepository<Cat> catRepository,
+            IMapper mapper)
+        {
+            _catRepository = catRepository;
+            _mapper = mapper;
+        }
+        public async Task AddAsync(CatDTO name)
+        {
+            var cat = _mapper.Map<Cat>(name);
+            await _catRepository.AddAsync(cat);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            await _catRepository.DeleteByIdAsync(id);
+        }
+
+        public async Task<List<CatDTO>> GetAllAsync()
+        {
+            var cats = await _catRepository.GetAllAsync();
+            return _mapper.Map<List<CatDTO>>(cats);
+        }
+
+        public async Task<CatDTO> GetByIdAsync(int id)
+        {
+            var car = await _catRepository.GetByIdAsync(id);
+            return _mapper.Map<CatDTO>(car);
+        }
+
+        public async Task<List<CatDTO>> GetCarByBrandAsync(string breed)
+        {
+            var breedCats= await _catRepository.GetByFilterAsync(cat => cat.Breed == breed);
+            return _mapper.Map<List<CatDTO>>(breedCats);
+        }
+
+        public async Task UpdateAsync(CatDTO name)
+        {
+            var cat = _mapper.Map<Cat>(name);
+            await _catRepository.UpdateAsync(cat);
+        }
+    }
+}
---
+++ src/FindAFelineApp.Services/DTOs/CatDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAFelineApp.Services.DTOs
+{
+    public class CatDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public string Personality { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
