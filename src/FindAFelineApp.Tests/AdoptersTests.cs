f59c173718b6fe6e92d5dec3cf4f420a3a77d2f8
Alisa1206-morph <alisa6efremenko2007@gmail.com>
2025-05-04 14:37:51 +0300

    added AdoptersTests to project

---
+++ src/FindAFelineApp.Tests/AdoptersTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindAFelineApp.Data;
+using FindAFelineApp.Data.Entities;
+using FindAFelineApp.Services.Abstractions;
+using FindAFelineApp.Services.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+// need to add tests for adopter controller
+[TestClass]
+    public class AdoptersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
