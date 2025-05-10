639c63461fae7c31fe24ffb8238b0e060c4d7a1f
yansgg323 <yanagencheva32@gmail.com>
2025-05-10 14:45:16 +0300

    chnages to cards

---
+++ src/FindAFelineApp.Data/Repositories/Abstractions/ICatRepository.cs
@@ -0,0 +1,13 @@
+﻿using FindAFelineApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAFelineApp.Data.Repositories.Abstractions
+{
+    public interface ICatRepository : ICrudRepository<Cat>
+    {
+    }
+}
