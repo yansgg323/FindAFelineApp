b8e803c70e932322dcdb42e1973c0037200975c3
yansgg323 <yanagencheva32@gmail.com>
2025-05-10 16:12:08 +0300

    add migrations

---
+++ src/FindAFelineApp.Data/Migrations/20250510131104_FosterParentSchemas.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindAFelineApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FosterParentSchemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FosterParentId",
+                table: "Cats",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cats_FosterParentId",
+                table: "Cats",
+                column: "FosterParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cats_FosterParents_FosterParentId",
+                table: "Cats",
+                column: "FosterParentId",
+                principalTable: "FosterParents",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cats_FosterParents_FosterParentId",
+                table: "Cats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cats_FosterParentId",
+                table: "Cats");
+
+            migrationBuilder.DropColumn(
+                name: "FosterParentId",
+                table: "Cats");
+        }
+    }
+}
